94f4f93683fc2c94f456ed914044a5eb6fbb4750
aknkya <akn_kya@hotmail.com>
2021-08-15 20:15:25 +0300

    Path Update

--- src/main/java/provision/com/example/provision/demo/Config/MvcConfig.java
+++ src/main/java/provision/com/example/provision/demo/Config/MvcConfig.java
@@ -1,19 +1,29 @@
 package provision.com.example.provision.demo.Config;
 
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+
 import java.io.File;
+import java.nio.file.FileSystems;
 
 @Configuration
 @EnableWebMvc
 public class MvcConfig extends WebMvcConfigurerAdapter {
+    String userDirectory = FileSystems.getDefault()
+            .getPath("")
+            .toAbsolutePath()
+            .toString();
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry
                 .addResourceHandler("/images/**")
-                .addResourceLocations(new File("C:\\Users\\akn_k\\Desktop\\provision.demo").toURI().toString());
+                .addResourceLocations(new File(userDirectory).toURI().toString());
+
+
+
     }
 }
